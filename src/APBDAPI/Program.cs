59d7dfe304267caa9008d898b4b2712565e8dae6
marczakjulia <s31153@pjwstk.edu.pl>
2025-04-09 21:36:26 +0200

    added more seperation in logic and updated the program with http codes and catching possible errors :)

--- src/APBDAPI/Program.cs
+++ src/APBDAPI/Program.cs
@@ -16,45 +16,127 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
 var deviceManager = DeviceManagerCreation.CreateDeviceManager("/Users/juliamarczak/RiderProjects/APBD_5/src/APBDLogic /input.txt");
 
-app.MapGet("/api/devices", () => deviceManager.GetAllDevices());
+app.MapGet("/api/devices", () =>
+{
+    try
+    {
+        return Results.Ok(deviceManager.GetAllDevices());
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem("Error with getting all devices", statusCode: 500);
+    }
+});
 
-app.MapGet("/api/devices/{id}", (string id) => deviceManager.GetDeviceById(id));
+app.MapGet("/api/devices/{id}", (string id) =>
+{
+    try
+    {
+        var device = deviceManager.GetDeviceById(id);
+        if (device == null)
+        {
+            return Results.NotFound("Device not found");
+        }
+        return Results.Ok(device);
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem("Error occured when finding device", statusCode: 500);
+    }
+});
 
 app.MapPost("/api/devices/pc", (PersonalComputer pc) =>
 {
-    deviceManager.AddDevice(pc);
+    try
+    {
+        deviceManager.AddDevice(pc);
+        return Results.Created($"/api/devices/{pc.Id}", pc);
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem("Error when adding device", statusCode: 500);
+    }
 });
 
 app.MapPost("/api/devices/smartwatch", (Smartwatch watch) =>
 {
-    deviceManager.AddDevice(watch);
+    try
+    {
+        deviceManager.AddDevice(watch);
+        return Results.Created($"/api/devices/{watch.Id}", watch);
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem("Error when adding device", statusCode: 500);
+    }
 });
 
 app.MapPost("/api/devices/embedded", (Embedded ed) =>
 {
-    deviceManager.AddDevice(ed);
+    try
+    {
+        deviceManager.AddDevice(ed);
+        return Results.Created($"/api/devices/{ed.Id}", ed);
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem("Error when adding device", statusCode: 500);
+    }
 });
 
 app.MapPut("/api/devices/pc", (PersonalComputer pc) =>
 {
-    deviceManager.EditDevice(pc);
+    try
+    {
+        deviceManager.EditDevice(pc);
+        return Results.NoContent();
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem("Error when updating device", statusCode: 500);
+    }
 });
 
 app.MapPut("/api/devices/smartwatch", (Smartwatch sw) =>
 {
-    deviceManager.EditDevice(sw);
+    try
+    {
+        deviceManager.EditDevice(sw);
+        return Results.NoContent();
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem("Error when updating device", statusCode: 500);
+    }
 });
 
 app.MapPut("/api/devices/embedded", (Embedded ed) =>
 {
-    deviceManager.EditDevice(ed);
+    try
+    {
+        deviceManager.EditDevice(ed);
+        return Results.NoContent();
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem("Error when updating device", statusCode: 500);
+    }
 });
 
 app.MapDelete("/api/devices/{id}", (string id) =>
 {
-    deviceManager.RemoveDeviceById(id);
+    try
+    {
+        deviceManager.RemoveDeviceById(id);
+        return Results.NoContent();
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem("Error when deleting device", statusCode: 500);
+    }
 });
 
 app.Run();
